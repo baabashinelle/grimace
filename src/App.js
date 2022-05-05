263f440a8889004b02e606c9c73bce4fbc40fd6d
Baaba <baabadampare@gmail.com>
2022-05-05 11:48:38 +0000

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../assets/websitemenu-logo.svg';
+
+function Header() {
+  return (
+    <div>
+      <div className="flex justify-between px-10 pt-5">
+        <img src={logo} alt="Grimace Bridge Logo" />
+        <button className="border-2 border-[#511597] rounded-full px-5 text-[#511597]">
+          Connect Wallet
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Header
