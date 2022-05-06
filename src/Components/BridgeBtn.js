11dfe2d7171a155410f5616e42db31a683bf1d4e
Baaba <baabadampare@gmail.com>
2022-05-06 20:48:17 +0000

    edit modals

--- src/Components/BridgeBtn.js
+++ src/Components/BridgeBtn.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { MdTouchApp } from "react-icons/md";
 
 function BridgeBtn() {
   const [showModal, setShowModal] = useState(false);
@@ -14,57 +15,32 @@ function BridgeBtn() {
       </button>
       {showModal ? (
         <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-          >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+          <div className="justify-center items-center flex fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative">
+              <button
+                className="bg-[#E1D1F3] text-[#A05AF0] rounded-xl p-1"
+                onClick={() => setShowModal(false)}
+              >
+                x
+              </button>
               {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    Modal Title
-                  </h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
-                {/*body*/}
+              <div className="border-0 rounded-2xl relative flex w-full bg-[#E1D1F3] outline-none focus:outline-none">
                 <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
-                  </p>
-                </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
-                  </button>
+                  <div className="text-[#A05AF0] text-left font-bold flex">
+                    <MdTouchApp className="text-2xl" /> Note
+                  </div>
+                  <ul className="text-left text-[#A05AF0] pt-4">
+                    <li>
+                      The tokens will be sent to the same address they were sent
+                      from
+                    </li>
+                    <li className="pt-3">Minimum deposit is 0.02 ETH</li>
+                  </ul>
                 </div>
               </div>
             </div>
           </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+          <div className="opacity-[0.9] fixed inset-0 z-40 bg-black"></div>
         </>
       ) : null}
     </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,59 @@
 import React from 'react';
+import { useState } from "react";
 import logo from '../assets/websitemenu-logo.svg';
+import metamask from "../assets/metamask.svg";
+import walletconnect from "../assets/walletConnect.svg";
 
 function Header() {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div>
       <div className="flex justify-between px-10 pt-5">
         <img src={logo} alt="Grimace Bridge Logo" />
-        <button className="border-2 border-[#511597] rounded-full px-7 text-[#511597] h-9">
+        <button
+          className="border-2 border-[#511597] rounded-full px-7 text-[#511597] h-9"
+          onClick={() => setShowModal(true)}
+        >
           Connect Wallet
         </button>
+
+        {showModal ? (
+          <>
+            <div className="justify-center items-center flex fixed inset-0 z-50 outline-none focus:outline-none">
+              <div className="relative">
+                {/*content*/}
+                <div className="border-0 rounded-2xl relative flex bg-[#E1D1F3] outline-none focus:outline-none w-[63%]">
+                  <div className="relative p-6 flex-auto">
+                    <div className="text-[#A05AF0] text-left font-bold flex justify-between border-b-2 border-[#DAB9FF] p-1 ">
+                      Connect Wallet
+                      <span
+                        className="text-[#6609D5] bg-white rounded-full p1 cursor-pointer"
+                        onClick={() => setShowModal(false)}
+                      >
+                        x
+                      </span>
+                    </div>
+                    <div className="text-[#A05AF0] font-thin text-sm bg-white p-4 rounded-xl text-left">
+                      By connecting a wallet, you agree to Grimace's{" "}
+                      <span className="font-bold">Terms of Service</span> and
+                      acknowledge that you have read and understood the
+                      Grimace's disclaimer
+                    </div>
+                    <div className="flex text-[#A05AF0] pb-5 gap-3 pt-9 focus:bg-[#F7F0FF] cursor-pointer">
+                      <img src={metamask} alt="Metamask" />
+                      MetaMask
+                    </div>
+                    <div className="flex text-[#A05AF0] gap-3 cursor-pointer">
+                      <img src={walletconnect} alt="Wallet Connect" />
+                      WalletConnect
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="opacity-[0.9] fixed inset-0 z-40 bg-black"></div>
+          </>
+        ) : null}
       </div>
     </div>
   );
