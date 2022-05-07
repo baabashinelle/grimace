cbcbb5d39bee664e1158ba310a81b164c71615f2
Baaba <baabadampare@gmail.com>
2022-05-07 20:08:46 +0000

    fix logo in header

--- src/Components/DropdownOption.js
+++ src/Components/DropdownOption.js
@@ -43,19 +43,19 @@ function DropdownOption() {
         <>
           <div className="mt-1 relative">
             <Listbox.Button
-              className={`relative w-full bg-[#F3E8FF] border rounded-md pl-3 pr-10 py-2 text-left cursor-pointer focus:outline-[#772EDA] focus:ring-1 focus:ring-[#772EDA] focus:border-[#772EDA] sm:text-sm ${
+              className={`relative w-full bg-[#F3E8FF] h-[3rem] overflow-hidden flex items-center border rounded-md pl-3 pr-10 py-2 text-left cursor-pointer focus:outline-[#772EDA] focus:ring-1 focus:ring-[#772EDA] focus:border-[#772EDA] sm:text-sm ${
                 selected.id == 0 && "pl-2"
               }`}
             >
               <span
                 className={`flex items-center gap-3 pl-2 ${
-                  selected.id == 0 && "gap-0 pl-[6px]"
+                  selected.id == 0 && "gap-0 pl-[3px]"
                 }`}
               >
                 <img
                   src={selected.image}
                   alt=""
-                  className={`rounded-full ${selected.id == 0 && "w-[2.5rem]"}`}
+                  className={`rounded-full ${selected.id == 0 && "w-[2.7rem]"}`}
                 />
                 <span className="text-base text-[#511597]">
                   {selected.name}
